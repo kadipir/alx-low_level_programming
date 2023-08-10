6caa64c897523a542544578b5d820e210abbac73
kadi <trutherkadi@gmail.com>
2023-08-10 13:57:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * tis function is the entry point to the program
+ * main - program that outputs a line with puts functiom
+ * Returns = 0 (success)
+ */
+int main(void)
+{char message[] = "Programming is like building a multilingual puzzle";
+puts(message);
+return (0);
+}
