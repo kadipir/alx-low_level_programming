eb2774124a921e19d76c796945c1ef93dfe2966c
kadi <trutherkadi@gmail.com>
2023-08-10 16:46:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: 1 (Success)
+ * Return: 1 done
  */
 int main(void)
-{fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+{fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
 return (1);
 }
