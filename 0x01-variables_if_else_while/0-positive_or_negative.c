b12009e1b2d78acebfd0a3ad0221e6ee709fc504
kadi <trutherkadi@gmail.com>
2023-08-11 03:23:13 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-{printf("is positive"); }
+{printf("98 is positive"); }
 else if (n == 0)
-{printf("is zero"); }
+{printf("0 is zero"); }
 else if (n <= 0)
-{printf("is negative"); }
+{printf("-98 is negative"); }
 return (0);
 }
