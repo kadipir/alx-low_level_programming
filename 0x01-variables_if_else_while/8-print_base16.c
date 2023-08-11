16bcfcea5c2c6dc8c0a050f5c0f836b5059a9182
kadi <trutherkadi@gmail.com>
2023-08-11 13:23:01 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{char n, y;
+for (n = '0' ; n <= '9' ; n++)
+putchar(n);
+for (y = 'a' ; y <= 'f' ; y++)
+putchar(y);
+putchar('\n')
+return (0);
+}
