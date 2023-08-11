d58d9f67e335db860e978af352778cc6e4fcec7b
kadi <trutherkadi@gmail.com>
2023-08-11 13:51:03 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,11 @@
 int main(void)
 {int c;
 for (c = 0 ; c <= 9 ; c++)
-putchar((c % 10) + '0');
+{putchar((c % 10) + '0');
 if (c <= 8)
 {putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
