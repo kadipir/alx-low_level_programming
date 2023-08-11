1d430bb5cddc1360f48e1be4c153c73937564e55
kadi <trutherkadi@gmail.com>
2023-08-11 12:36:13 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{int x;
+for (x = 0 ; x < 10 ; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
