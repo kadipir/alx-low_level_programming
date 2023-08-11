8d74c0adde0f919f54aa9e4bf5e931964039853d
kadi <trutherkadi@gmail.com>
2023-08-11 14:21:57 -0700

    % 10) + '10')

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,10 +7,11 @@
 int main(void)
 {int x, y;
 for (x = 0 ; x < 10 ; x++)
-{for (y = 0 ; y < 10 ; y++)
+{
+for (y = 0 ; y < 10 ; y++)
 if (x < y)
-{{putchar(x);
-putchar(y);
+{{putchar((x % 10) + '10');
+putchar((y % 10) + '10');
 if (x != 8 || (x == 8 && y != 9))
 {putchar(',');
 putchar(' ');
