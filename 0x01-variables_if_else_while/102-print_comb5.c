58767256edc06370bbb30db21f549654517a48ac
kadi <trutherkadi@gmail.com>
2023-08-11 23:40:29 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,11 +11,14 @@ for (x = 0 ; x < 100 ; x++)
 for (y = 0 ; y < 100 ; y++)
 {
 if (x < y)
+{
 putchar((x % 10) + '0');
-putchar(' ');
 putchar((y % 10) + '0');
+putchar(' ')
 if (x != 98 && y != 99)
 {putchar(',');
+putchar(' ');
+}
 }
 }
 }
