e2ffedc5fab608c5a41a2d7b773b520a83980b92
kadi <trutherkadi@gmail.com>
2023-08-11 23:47:33 -0700

    putchar((x / 10) + '0');

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,11 @@ for (y = 0 ; y < 100 ; y++)
 {
 if (x < y)
 {
+putchar((x / 10) + '0');
 putchar((x % 10) + '0');
-putchar((y % 10) + '0');
 putchar(' ');
+putchar((y / 10) + '0');
+putchar((y % 10) + '0');
 if (x != 98 || y != 99)
 {putchar(',');
 putchar(' ');
