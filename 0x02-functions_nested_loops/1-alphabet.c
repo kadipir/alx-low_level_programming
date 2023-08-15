aeda2df417a058f39e7f00728ca48289b2732de8
kadi <trutherkadi@gmail.com>
2023-08-15 11:13:30 -0700

    print_alphabet(void)

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{char a
+for(a = 'a' ; a <= 'z' ; a--)
+{
+_putchar(a);	
+_putchar('\n');
+}
+}
+int main(void)
+{print_alphabet(void);
+return (0);
+}
