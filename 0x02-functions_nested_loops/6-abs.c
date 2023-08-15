b0fd3acf13e74989e3e985dfe86a80c07db61a7e
kadi <trutherkadi@gmail.com>
2023-08-15 14:14:27 -0700

    int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of an integer
+ * @c: the integer to be checked
+ * Return:absolute number of integer or zero
+ */
+int _abs(int)
+{
+	if (c < 0)
+	{
+		int = abs_value;
+		abs_value = c * -1;
+		return (abs_value);
+	}
+	return (c);
+}
