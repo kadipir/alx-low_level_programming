e6b9a4f9572bb8007f701098cdd622c60dbd65dc
kadi <trutherkadi@gmail.com>
2023-08-15 16:49:07 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,12 +5,19 @@
  */
 void print_to_98(int n)
 {
-	for (n = 0 ; n < 99 ; n++)
-	{
-	  _putchar(n);
-	  _putchar(',');
-	  _putchar(' ');
-	}
-	_putchar('\n');
+for (; n < 99 ; n++)
+{
+_putchar(n + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+for (; n >= 98 ; n--)
+{
+_putchar(n + 'o');
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n')
 }
 
