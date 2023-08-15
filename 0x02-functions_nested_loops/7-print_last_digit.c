7ff8d068485a21c066f25a500dfee38108e9ffa1
kadi <trutherkadi@gmail.com>
2023-08-15 14:43:34 -0700

    last:wq

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,15 @@
 /**
  * print_last_digit - print last digit of a number
  * @c: number to be checked
- * Return: value of last digit or 0 if any other 
+ * Return: value of last digit or 0 if any other
  */
 int print_last_digit(int c)
-{  
+{
 int n = c % 10;
 if (n < 0)
 {
-n = n * -1;	 
+n = n * -1;
 }
 _putchar(n + '0');
-return (n);	     
+return (n);
 }
