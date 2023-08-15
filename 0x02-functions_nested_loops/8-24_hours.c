fec0730587c68ddac3609b805685ca11c3be3e87
kadi <trutherkadi@gmail.com>
2023-08-15 15:23:21 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 int a, b, c, d;
 for (a = 0 ; a < 3 ; a++)
 {
-for (b =0 ; b < 10 : b++)
+for (b =0 ; b < 10 ; b++)
 {
-if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3)
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
 for (c = 0 ; c < 6 ; c++)
 {
