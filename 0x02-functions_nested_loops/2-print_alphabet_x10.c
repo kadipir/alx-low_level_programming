12c7ec348f63bbe6f5a234d03e85d33d40050ded
kadi <trutherkadi@gmail.com>
2023-08-15 12:07:24 -0700

    void print_alphabet_x10(void);

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 /**
  * print_alphabet_x10 - print the alphabet 10 times
  * return: void
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,1 @@
-int _putchar(char chara);
-void print_alphabet(void);
+void print_alphabet_x10(void);
