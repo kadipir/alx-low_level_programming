dce01bf56318cfbe160bc9414744cb054ee36a44
kadi <trutherkadi@gmail.com>
2023-08-15 06:26:02 -0700

    _putchar('-');

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,13 @@
 int main(void)
 {
 	_putchar('n');
+	_putchar('o');
+	_putchar('m');
+	_putchar('v');
+	_putchar('0');
+	_putchar('-');
+	_putchar('#');
+	_putchar('*');
 	_putchar('\n');
 	return (0);
 }
