ed5f806ae7ab3425a6d7724f090e85db1d4d5f14
kadi <trutherkadi@gmail.com>
2023-08-15 15:20:01 -0700

    _putchar(a + '0');

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,29 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of jack_bauer
- * returns ...
+ *
+ *
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
-	for (a = 0 ; a <=2 ; a++)
-	{
-		for (b = 0 ; b < 10 ; b++)
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0 ; c < 6 ; c++
-						{
-						for (d = 0 ; d < 10 ; d++)
-						{
-						 _putchar(a + '0');
-						 _putchar(b + '0');
-						 _putchar(58);
-						 _putchar(c + '0');
-						 _putchar(d + '0');
-
-						}
-						}
-			}
-	}
+int a, b, c, d;
+for (a = 0 ; a < 3 ; a++)
+{
+for (b =0 ; b < 10 : b++)
+{
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3)
+{
+for (c = 0 ; c < 6 ; c++)
+{
+for (d = 0 ; d < 10 ; d++)
+{
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(58);
+_putchar(c + '0');
+_putchar(d + '0');
+}
+}
+}
+}
+}
 }
