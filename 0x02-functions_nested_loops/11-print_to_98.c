f043d060d2d608af88fcf7c5226486f888edb28d
kadi <trutherkadi@gmail.com>
2023-08-15 16:32:43 -0700

    listing to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbers to 98
+ * Return: no value
+ */
+void print_to_98(int n)
+{
+	int x;
+       
+	for (x = 0 ; x < 99 ; x++}
+	{
+	  _putchar(x);
+	  _putchar(',');
+	  _putchar(' ');
+	}
+	_putchar('\n');
+}
+
