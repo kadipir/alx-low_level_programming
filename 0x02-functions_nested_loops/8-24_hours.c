c0988a8fb116d408bc16f4f94194b1286f760a91
kadi <trutherkadi@gmail.com>
2023-08-15 15:26:32 -0700

    jack_bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *
- *
+ *jack_bauer - prints every minute of the day of jack_bauer
+ *Return ...
  */
 void jack_bauer(void)
 {
 int a, b, c, d;
 for (a = 0 ; a < 3 ; a++)
 {
-for (b =0 ; b < 10 ; b++)
+for (b = 0 ; b < 10 ; b++)
 {
 if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
@@ -21,6 +21,7 @@ _putchar(b + '0');
 _putchar(58);
 _putchar(c + '0');
 _putchar(d + '0');
+_putchar('\n')
 }
 }
 }
