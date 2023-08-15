82750043f94760b96da0d3eb27ed1706edb18330
kadi <trutherkadi@gmail.com>
2023-08-15 14:17:38 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,11 +4,11 @@
  * @c: the integer to be checked
  * Return:absolute number of integer or zero
  */
-int _abs(int)
+int _abs(int c)
 {
 	if (c < 0)
 	{
-		int = abs_value;
+		int abs_value;
 		abs_value = c * -1;
 		return (abs_value);
 	}
