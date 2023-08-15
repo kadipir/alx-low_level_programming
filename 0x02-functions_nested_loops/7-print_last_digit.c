0ca4b9ab5924eedce1cd47a51544eb9ec12dd0db
kadi <trutherkadi@gmail.com>
2023-08-15 14:39:38 -0700

    #include main.h

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,12 @@
  * Return: value of last digit or 0 if any other 
  */
 int print_last_digit(int c)
-{  int n = c % 10;
-         if (n < 0)
-	 {
-              n = n * -1;	 
-
-	 }
-     _putchar(n + '0');
-    return (n);	     
+{  
+int n = c % 10;
+if (n < 0)
+{
+n = n * -1;	 
+}
+_putchar(n + '0');
+return (n);	     
 }
