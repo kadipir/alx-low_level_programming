96ccd2e2782cd71b9621d9a86bb87b65a92bbaa8
kadi <trutherkadi@gmail.com>
2023-08-16 23:59:37 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void more_numbers(void)
+{
+int a, b;
+for (a = 0 ; a < 10 ; a++)
+{
+for(b = 0 ; b < 15 ; a++)
+{
+_putchar(b + '0');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
