e30674bb4b2fc4093da293bf7948db86851137fe
kadi <trutherkadi@gmail.com>
2023-08-17 00:46:32 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib>
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+int a;
+for (a = 1 ; a < 101 ; a++)
+{
+if (a % 3 = 0)
+{
+printf("fizz");
+}
+else if (a % 5 = 0)
+{
+printf("buzz");
+}
+else if (a % 15 = 0)
+{
+printf(""fizzbuzz);
+}
+printf(a);
+}
+printf('\n');
+return (0);
+}
+
