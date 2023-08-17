0d4501c084cd05c7b1f2b4f8db0639ba7e11c3f6
kadi <trutherkadi@gmail.com>
2023-08-17 00:09:26 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void print_line(int n)
+{
+for (n = 0 ; n <= 11; n++)
+{
+if (n > 0)
+{
+_putchar('_');
+}
+else
+{
+_putchar("\n");
+}
+}
+_putchar('\n');
+}
