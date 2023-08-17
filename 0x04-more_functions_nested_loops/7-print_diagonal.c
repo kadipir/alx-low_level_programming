cdc73d737139b7fed68dbe5f0ea8d8f95c123b1a
kadi <trutherkadi@gmail.com>
2023-08-17 00:18:49 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void print_diagonal(int n)
+{
+for (n = 0 ; n < 11 ; n++)
+{
+if (n > 0)
+{
+_putchar('\\');
+_putchar('\n');
+}
+else
+{
+_putchar("\n");
+}
+}
+}
