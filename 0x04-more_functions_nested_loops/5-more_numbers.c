e7d72691f6f5533e41fd12c295dd818a658e7ccf
kadi <trutherkadi@gmail.com>
2023-08-17 03:59:25 -0700

    _putchar((b / 10) + '0');

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,15 @@ for (a = 0 ; a < 10 ; a++)
 {
 for (b = 0 ; b < 15 ; a++)
 {
-if (b > 9)
-{
-_putchar((b / 10) + '0');
-_putchar((b % 10) + '0');
-}
-else
+if (b < 10)
 {
 _putchar(b + '0');
 }
+else
+{
+_putchar((b / 10) + '0');
+}
+_putchar((b % 10) + '0');
 }
 _putchar('\n');
 }
