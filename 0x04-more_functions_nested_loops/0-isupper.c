f33ebf1eb4121fff3844553f32406830bb777cdd
kadi <trutherkadi@gmail.com>
2023-08-16 23:19:58 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase characters
+ * @c - the character to be checked
+ * return: 1 (successfull) 0 if otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
+
