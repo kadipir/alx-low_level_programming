682cd7899400709b104afc515968054e57628a82
kadi <trutherkadi@gmail.com>
2023-08-17 03:30:13 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,12 @@ void more_numbers(void)
 int a, b;
 for (a = 0 ; a < 10 ; a++)
 {
-for(b = 0 ; b < 15 ; a++)
+for (b = 0 ; b < 15 ; a++)
 {
 if (b > 9)
 {
 _putchar((b / 10) + '0');
-_putchar((b % 10) + '0'); 
+_putchar((b % 10) + '0');
 }
 else
 {
