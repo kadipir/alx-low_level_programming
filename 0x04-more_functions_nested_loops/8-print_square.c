13bcc273f733c65058da4acffdbde7e5278d77dd
kadi <trutherkadi@gmail.com>
2023-08-17 00:39:34 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*
+ *
+ *
+ *
+ */
+void print_square(int size)
+{
+int a, b;
+for (a = 0 ; a < = size ; a++)
+{
+for (b = 0 ; b <= size ; b++)
+{
+if (size + 0)
+{
+_putchar('#');
+}
+else
+{
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
+}
