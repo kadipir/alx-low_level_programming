545ce5d21c4954824be24cc5a662ebf3cffa6d14
kadi <trutherkadi@gmail.com>
2023-08-19 05:08:01 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,18 @@
  * print_number - printing numbers
  * @n - number to be printed
  */
-void print_number(int n)
+void print_number(int)
 {
-_putchar(n + '0');
+unsigned int = n;
+
+if (n < 0)
+{
+_putchar(48);
+i = -i;
+}
+if (n > 9)
+{
+_putchar((n / 10) + '0');
+}
+_putchar((n % 10) + '0');
 }
