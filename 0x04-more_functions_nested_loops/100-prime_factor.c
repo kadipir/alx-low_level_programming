c77d979168bb80985ef3219cc7118ad5b7029a76
kadi <trutherkadi@gmail.com>
2023-08-19 04:17:17 -0700

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+long int a, b, c;
+
+a =  612852475143;
+for (b = 1 ; b <= a ; b++)
+{
+if (a % b == 0)
+{
+if (a == b)
+{
+printf("%ld\n", b);
+}
+}
+else
+{
+c = a / b;
+a = c;
+}
+}
+return (0);
+}
