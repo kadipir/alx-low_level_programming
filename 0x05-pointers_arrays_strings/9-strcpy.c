a7dfb19b59f42e08126a1308b96ed5978ad9a544
kadi <trutherkadi@gmail.com>
2023-08-21 15:55:56 -0700

    9-copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,6 +9,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-copied = strcpy(*desr *src);
-return (copied);
+int i;
+
+for (i = 0 ; src[i] != '\0' ; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
 }
