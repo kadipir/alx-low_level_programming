b992ad14a87bbd692d153996c8d3a9bfa6bd16b2
kadi <trutherkadi@gmail.com>
2023-08-21 12:48:44 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 /**
  * _strlen - checks the length of a string
  * @s: string to be checked
- * return: length of the string
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
 int string_l;
 
 string_l = 0;
-while(s[string_l])
+while (s[string_l])
 	string_l++;
 return (string_l);
 }
