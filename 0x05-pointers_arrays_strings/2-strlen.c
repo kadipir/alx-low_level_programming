7fb34104e4df0ec32b3ed107e788d1e89c419186
kadi <trutherkadi@gmail.com>
2023-08-21 12:01:14 -0700

    int _strlen(char *s)

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - finds the length of a string
+ * @s: string whose length is evaluated
+ * return: len
+ */
+int _strlen(char *s)
+{
+int = len;
+len = strlen(str);
+return (len);
+}
