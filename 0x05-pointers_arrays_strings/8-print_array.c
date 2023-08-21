eb6ef5fcf8be522e84c184fa0ae085f84f2689bd
kadi <trutherkadi@gmail.com>
2023-08-21 15:33:25 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,13 +9,13 @@ void print_array(int *a, int n)
 {
 int x;
 
-*a = n;
-
 for (x = 0 ; x < n ; x++)
 {
-printf("%d", x);
-_putchar(',');
-_putchar(' ');
+printf("%d", a[x]);
+if (x != (n - 1))
+{
+_putchar(', ');
+}
 }
 _putchar('\n');
 }
