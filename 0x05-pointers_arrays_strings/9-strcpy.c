bacaf5f29fc2164ad0a468f9a9753974fa6a9ea1
kadi <trutherkadi@gmail.com>
2023-08-21 15:49:05 -0700

    9-copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copy strings fron=m one address to another 
+ * @dest: the first address
+ * @src:second address
+ * Return: value that is copied
+ */
+char *_strcpy(char *dest, char *src)
+{
+copied = strcpy(*desr *src);
+return (copied);
+}
