49b0e31c8f0187dd0703f99258b84abb92a02f34
kadi <trutherkadi@gmail.com>
2023-08-21 13:00:22 -0700

    puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * -puts - prints the string and the new line
+ *  @str: pointer to the string
+ * Return:string and new line
+ */
+void _puts(char *str)
+{
+int i;
+
+i = 0;
+while (str[i])
+	_putchar(str[i]);
+        i++;
+_putchar('\n');
+}
