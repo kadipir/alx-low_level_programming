23cbdc8582fdefe53ab24eb8c304e86453da8aa0
kadi <trutherkadi@gmail.com>
2023-08-22 11:39:50 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,8 +6,9 @@
 int _atoi(char *s)
 {
 int result = 0;
-int sign = 1;
-
+int sign = 1
+while (*s != '\0')
+{
 if (*s == '-' || *s == '+')
 {
 if (*s == '-')
@@ -16,8 +17,6 @@ sign = -1;
 }
 s++;
 }
-while (*s != '\0')
-{
 if (*s > '0' || *s < 10)
 {
 result = result * 10 + (*s - '0');
