a1fdacaf0b37c84947773cb00215a8ee4c893694
kadi <trutherkadi@gmail.com>
2023-08-24 04:51:26 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-strcat (*dest *src);
-int *cat = dest;
-return (*cat);
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++
+}
+*desti = '0'
+int *desti = desti;
+return (desti)
 }
