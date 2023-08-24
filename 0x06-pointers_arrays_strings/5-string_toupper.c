b755aa63734de750b2ffcb81571f54408a406cf5
kadi <trutherkadi@gmail.com>
2023-08-24 12:43:32 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper - converts lower to uppercase
+ * @*: pointer to anull terminated string
+ * Return: the resultant string 
+ */
+char *string_toupper(char *)
+{
+char *string = upper
+while (*string != '\0')
+{
+if (islower(*string))
+{
+*string = toupper(*string);
+}
+string++;
+}
+return (upper);
+}	
