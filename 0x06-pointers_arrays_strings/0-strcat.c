792edab1648a2b96b87c5b6fc462140cfc02683a
kadi <trutherkadi@gmail.com>
2023-08-24 04:54:50 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings
- * *dest: first string
- * *src: string to be copied
+ * @_strcat - concatenates two strings
+ * @dest: first string
+ * @src: string to be copied
+ * Return: pointer to the resultant string
  */
 char *_strcat(char *dest, char *src)
 {
