a36ae4b9ae568e43247f095e2f3aae9a3fe75637
kadi <trutherkadi@gmail.com>
2023-08-24 11:54:34 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,30 +7,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 != '\0' && *s2 != '\0')
+while (*s1 &&* s2 && *s1 == *s2)
 {
-if (*s1 > *s2)
-{
-return (1);
-}
-else if (*s1 < *s2)
-{
-return (-1);
-}
 s1++;
 s2++;
 }
-
-if (*s1 != '\0' && *s2 != '\0')
-{
-return (0);
-}
-else if (*s1 != '\0')
-{
-return (-1);
-}
-else
-{
-return (1);
-}
+return (*s1 - *s2);
 }
