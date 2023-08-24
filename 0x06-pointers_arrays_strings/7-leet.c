4ef707708ccae59097f8b1d5927a3c57ff97666d
kadi <trutherkadi@gmail.com>
2023-08-24 13:43:26 -0700

    e || E

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/** 
+ * *leet - converts characters to 1337
+ * @str: pointer to the 
+ * Return: 1337 (successful)
+ */
+char *leet(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (str[i] == {'l || L', 'e || E', 'e || E', 't || T'})
+i++
+}
+return (1337);
+}
+
