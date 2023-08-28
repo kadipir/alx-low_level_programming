b96f146c27b9f6411242899f98fdf892e8e87c95
truth <truth@gmail.com>
2023-08-28 12:35:47 -0700

    #include main.h

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copies a memory block
+ * @dest: destination 
+ * @src: source
+ * @n: bytes
+ * Return: dest (successfull)
+ */ 
+char *_memcpy(char *dest, char *src, unsigned int n)
+{       
+umsigned int i;
+
+for (i = 0 ; i < n ; i++)
+{
+dest[i] = src[i];
+}       
+return
+}       
