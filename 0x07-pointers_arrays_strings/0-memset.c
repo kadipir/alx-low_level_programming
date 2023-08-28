5c6276df92ff4e86cf15207343056ce5c44536ea
truth <truth@gmail.com>
2023-08-28 12:17:41 -0700

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * @_memset - entry point
+ * @s: pointer to the destination
+ * @b: character to be printed
+ * @n: bytes
+ * Return: s (successful)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0 ; i < n ; i++)
+{
+s[i] = b;
+}
+return (s);
+}
