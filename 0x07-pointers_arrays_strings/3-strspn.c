0989b044b0c6c5c9d39fabeb7f874fe35cfd8232
truth <truth@gmail.com>
2023-08-29 02:42:36 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * _strspn - entry point
- * @s: character examined
- * @accept: equal to *s
- * Return: 0 (successful)
- */ 
+ *
+ *
+ *
+ *
+ */
 unsigned int _strspn(char *s, char *accept)
-{       
+{
 int i, j, value, check;
 
 for (i = 0 ; s[i] != '\0' ; i++)
-{ 
 check = 0;
-
+{
 for (j = 0 ; accept[j] != '\0' ; j++)
 {
 if (accept[j] == s[i])
@@ -20,6 +19,8 @@ if (accept[j] == s[i])
 check = 1;
 value++;
 }
-}       
 }
-}       
+}
+return (s);
+}
+
