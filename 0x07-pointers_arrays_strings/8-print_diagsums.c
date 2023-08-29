f5fcd1b5c69f4c887d806457bb1e401d85af7657
truth <truth@gmail.com>
2023-08-29 11:12:26 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - entry point
+ * @a: input
+ * @size: input
+ */
+void print_diagsums(int *a, int size)
+{
+int i, sum1, sum2;
+
+for (i = 0 ; i < size ; i++)
+{
+sum1 += a[index];
+a += size;
+}
+
+a -= size;
+for (i = 0 ; i < size ; i++)
+{
+sum2 += a[index];
+a -= size;
+}
+printf("%d, %d\n", sum1, sum2);
+}
