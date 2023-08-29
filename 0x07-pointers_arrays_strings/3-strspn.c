de19a8b5304657b6aba701bac2b2aeb73f3c1047
truth <truth@gmail.com>
2023-08-29 02:46:07 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,6 +9,8 @@ unsigned int _strspn(char *s, char *accept)
 {
 int i, j, value, check;
 
+value = 0;
+
 for (i = 0 ; s[i] != '\0' ; i++)
 check = 0;
 {
@@ -16,11 +18,10 @@ for (j = 0 ; accept[j] != '\0' ; j++)
 {
 if (accept[j] == s[i])
 {
-check = 1;
 value++;
+check = 1;
 }
 }
 }
-return (s);
 }
 
