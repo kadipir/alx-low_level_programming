f6656651a3a00c5becdd8f38bec81951a39a2b33
truth <truth@gmail.com>
2023-08-30 10:50:25 -0700

    int _pow_recursion(int x, int y)

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - entry point
+ * @x: input
+ * @y: input
+ * Return: x to power y
+ */
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (1);
+}
+return (x ** _pow_recursion(x, y-1);
+}
