cb3fbf8c1ce45c366d4c9bb3b20aac3dde809f6c
truth <truth@gmail.com>
2023-09-04 05:58:34 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * create_array - entry point
+ * @size: bytes
+ * @c: input
+ */
+char *create_array(unsigned int size, char c)
+{
+_putchar(c);
+if (size <= 0)
+{
+return (NULL);
+}
+c = malloc (sizeof(size));
+return (s);
+}
