5592edc667005f9c0b7e05cc6881d0d4d96af4be
truth <truth@gmail.com>
2023-09-04 10:29:58 -0700

    #include main.h

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strdup - entry point
+ * @str: input
+ * Return: pointer to the string
+ *
+ */
+char *_strdup(char *str)
+{
+#include "main.h"
+/**
+ * _strdup - entry point
+ * @str: input
+ * Return: pointer to the string
+ *
+ */
+char *_strdup(char *str)
+{
+int i, size;
+
+if (str == NULL)
+{
+return ((NULL);
+}
+
+int length = strlen(str);
+
+char *s;
+
+s = malloc((sizeof(char) * length) + 1);
+
+for (i = 0 ; i < length ; i++)
+{
+s[i] = str[i];
+}       
+}
+return (s);
+}
