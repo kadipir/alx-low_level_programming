551c35283dca5f2f52a1e0a3ef3888a975cb74e2
truth <truth@gmail.com>
2023-09-04 11:58:26 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,42 +3,35 @@
 /**
  * str_concat - entry point
  * @s1: input
- * s2: input
+ * @s2: input
  * Return: pointer to result
  */
 char *str_concat(char *s1, char *s2)
 {
 char *s;
 int i = 1, j = 1, k, l;
-
 if (s1 == NULL) 
 {
 s1 = "";
 }
-
 if (s2 == NULL)
 {
 s2 = "";
 }
-
 while (s1[i])
 {
 i++;
 }
-
 while (s2[j])
 {
 j++;
 }
-
 l = i + j;
 s = malloc((sizeof(char) * l) + 1);
-
 if (s == NULL)
 {
 return (NULL);
 }
-
 while (k < l)
 {
 if (k <= i)
@@ -52,7 +45,6 @@ j++;
 }
 k++;
 }
-
 s[k] = '\0';
 return (s);
 }
