1b2b2737663fcf1f16d237c2a3d919d1248c558b
truth <truth@gmail.com>
2023-09-05 03:51:59 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - entry point
+ * @width: input
+ * @height: input
+ * Return: pointer to the array
+ */
+int **alloc_grid(int width, int height)
+{
+int *i[width][height], l, k;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+if (i == NULL)
+{
+return (NULL);
+}
+i = malloc((sizeof(int)) * (width + height));
+for (l = 0 ; l < width ; l++)
+{
+for (k = 0 ; k < height ; K++)
+{
+i[l][k];
+}
+}
+return (i);
+}
