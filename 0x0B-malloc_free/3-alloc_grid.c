862d630df70fbdd11b83fd8f27b983e9ef33b363
truth <truth@gmail.com>
2023-09-05 05:39:56 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,14 +15,14 @@ if (z <= 0)
 {
 return (NULL);
 }
-i = malloc((sizeof(int)) * width);
+i = malloc((sizeof(int)) * height);
 if (i == NULL)
 {
 return (NULL);
 }
-for (l = 0 ; l < width ; l++)
+for (l = 0 ; l < height ; l++)
 {
-i[l] = malloc((sizeof(int)) * height);
+i[l] = malloc((sizeof(int)) * width);
 if (i[l] ==  NULL)
 {
 for (l-- ; l >= 0 ; l--)
@@ -33,9 +33,9 @@ free(i);
 return (NULL);
 }
 }
-for (l = 0 ; l < width ; l++)
+for (l = 0 ; l < height ; l++)
 {
-for (l = 0 ; k < height ; k++)
+for (l = 0 ; k < width ; k++)
 {
 i[l][k] = 0;
 }
