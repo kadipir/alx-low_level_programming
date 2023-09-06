8ea7f154badf9fa11534de358606cb0d829b0e2c
truth <truth@gmail.com>
2023-09-06 12:22:48 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 /**
  * main - entry point
- * @a: input
- * @b: input
+ * @argv: argument vector
+ * @argc: argument count
  * Return: 0 (successful)
  */
-int main( int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int n1 = 0, n2 = 0;
 if (argc == 3)
 {
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+printf("%d\n", n1 *n2);
 }
 else
 {
