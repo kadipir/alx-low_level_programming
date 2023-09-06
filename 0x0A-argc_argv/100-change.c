c05b90d76a2386a176f0c7ec4c7b47356a126370
truth <truth@gmail.com>
2023-09-06 13:46:43 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[])
+{
+int cents;
+if (argc > 2)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+cents = atoi(argv[1]);
+if (cents < 0)
+{
+printf("0\n");
+}
+printf("%d\n", cents);
+}
+return (0);
+}
