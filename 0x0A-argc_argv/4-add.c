0f8dd21dbfdd513bf3625b2d6983a5bcfead3324
truth <truth@gmail.com>
2023-09-06 13:26:57 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,20 +1,40 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
- *
+ * main - entry point
+ * @argc: argumet count
+ * @argv: argument vector
+ * return: 0 (success)
  */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 int i;
 unsigned int k, sum;
-char c;
+char *c;
 
 if (argc > 1)
 {
-for ()
+for (i = 0 ; i < argc ; i++)
+{
+c = argv[i];
+for (k = 0 ; k < strlen(c) ; k++)
+{
+if (c[k] < 48 || c[k] > 57)
+{
+printf("Error\n");
+return(1);
 }
+}
+sum += atoi(c);
+c++;
+}
+printf("%d\n", sum);
+}
+else
+{
+printf("0\n");
+}
+return (0);
 }
