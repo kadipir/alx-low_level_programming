c94e5972ad0644d98968380c9bb276c1eabc47b6
truth <truth@gmail.com>
2023-09-06 11:28:17 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * main - entry point
@@ -5,7 +6,7 @@
  * @argv: argument vecror
  * Return: 0 (Successfull)
  */ 
-int main(int argc, char argv)
+int main(int argc, char *argv[])
 {
 (void) argc;
 printf("%s\n", argv[0]);
