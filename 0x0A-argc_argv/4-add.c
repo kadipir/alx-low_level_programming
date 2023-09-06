d0857a8332c7a9b9341e161150ab6f7218745dd1
truth <truth@gmail.com>
2023-09-06 13:13:45 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int main(int argc, char **argv[])
+{
+int i;
+unsigned int k, sum;
+char c;
+
+if (argc > 1)
+{
+for ()
+}
+}
