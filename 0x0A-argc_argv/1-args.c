d10c2750bd0da29dcf21025cb69deee8782aa3f8
truth <truth@gmail.com>
2023-09-06 11:53:16 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (successfull)
+ */ 
+int main(int argc, char *argv[])
+{
+(void) argv[];
+printf("\d\n", argc);
+
+return (0);
+}
