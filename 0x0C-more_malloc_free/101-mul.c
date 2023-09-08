6125a128e8b32a284f0c61b2fd5d8917634351a3
truth <truth@gmail.com>
2023-09-08 13:41:47 -0700

    _putchar('E');

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
  */
 int main(int argc, char *argv[])
 {
