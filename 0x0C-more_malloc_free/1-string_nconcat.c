1bc72625bf00edb6a4438eb3ca22ae8c772ce892
truth <truth@gmail.com>
2023-09-08 11:05:59 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - entry point
+ * @s1: input
+ * @s2: input
+ * @n: input
+ * Return: pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i = 0, j = 0, k = 0, l = 0;
+char *c;
+if (s1 == NULL || s2 == NULL)
+{
+s1 = "";
+s2 = "";
+}
+while (si[i])
+{
+i++;
+}
+while (s2[j])
+{
+j++;
+}
+if (n >= j)
+{
+l = i + j;
+}
+else
+{
+l = i + n;
+}
+c = malloc (((sizeof(char) * l)) +1);
+if (c == NULL)
+{
+return (NULL);
+}
+while (k < l)
+{
+if (k <= i)
+{
+c[k] = s1[k]
+}
+if (k >= i)
+{
+c[k] = s2[j];
+}
+k++;
+}
+c[k] = '\0';
+return (c);
+}
+
