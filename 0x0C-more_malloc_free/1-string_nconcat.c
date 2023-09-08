2cc068572e2f4391c2be7a385591a74e836c571b
truth <truth@gmail.com>
2023-09-08 11:21:29 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i = 0, j = 0, k = 0, l = 0;
 char *c;
-if (s1 == NULL || s2 == NULL)
+if (s1 == NULL)
 {
 s1 = "";
+}
+if (s2 == NULL)
+{
 s2 = "";
 }
 while (s1[i])
