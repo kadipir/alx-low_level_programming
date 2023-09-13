51af9138b5aa35ddbc047085fa90a1856ba199ef
truth <truth@gmail.com>
2023-09-13 12:44:08 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,13 @@
  * @name: input
  * @f: pointer to function
  * Return: nothing
- **/
+ */
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
+{
 return (0);
+}
 
 f(name);
 }
