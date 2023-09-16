75a38cbfa896374959f6d519d16df9da70c529b7
truth <truth@gmail.com>
2023-09-16 12:16:03 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list sum;
-int i, total;
+unsigned int i, total;
 va_start(sum, n);
-for (i = n ; i > 0 ; i = va_arg(sum, int))
+for (i = n ; i > 0 ; i++)
 {
-total += i;
-printf("%d", total);
+total += va_arg(sum, int);
 }
-return (0);
+va_end(sum);
+return (total);
 }
