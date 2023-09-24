6107a705a67a037319ed7aac61995cbd6409e83c
truth <truth@gmail.com>
2023-09-24 12:51:23 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - entry point
+ * @h: pointer to list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t index = 0;
+
+while (h)
+if (h->str != NULL)
+{
+printf("[0] NIl\n");
+}
+else
+{
+printf("[%u], %s", h->len, h->str);
+h = h->next;
+index++;
+}
+return (index);
+}
