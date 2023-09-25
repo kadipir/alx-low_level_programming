98445044240ee7239233911422032ad9e40db327
truth <truth@gmail.com>
2023-09-24 19:59:41 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - entry point
+ * @h: pointer to list
+ * Return: count to list;
+ */
+size_t list_len(const list_t *h)
+{
+size_t index = o;
+
+while (h)
+{
+index++;
+h = h->next;
+}
+return (index);
+}
