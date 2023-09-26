88ac401398a922342941db6fb57993216c03b0ee
truth <truth@gmail.com>
2023-09-25 19:39:44 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * _listint - entry point
+ * print_listint - entry point
  * @h: pointer to list
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 size_t index = 0;
+
 while (h)
 {
 printf("%d\n", h->n);
