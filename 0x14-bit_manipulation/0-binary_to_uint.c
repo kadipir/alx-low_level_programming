a60c307d6a8f02dbbdf9c6c258322bc4ff172bf6
truth <truth@gmail.com>
2023-10-01 08:07:33 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - entry point
+ * @b: pointer to string containing binary
+ * Return: integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int x;
+unsigned int dec;
+
+if (!b)
+{
+return (0);
+}
+for (x = 0 ; b[x] != '\0' ; x++)
+{
+if (x < '0' || x > '1')
+{
+return (0);
+}
+dec = 2 * dec + (b[x] - '0');
+}
+return (dec);
+}
