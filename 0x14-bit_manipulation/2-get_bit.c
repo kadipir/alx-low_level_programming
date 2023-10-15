3a5eb8f10204d5cc175e4550e2cc2571f3d5f1c4
truth <truth@gmail.com>
2023-10-15 01:30:02 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/** 
+ * get_bit: entry point
+ * @n: input decimal
+ * @index: position to be retrieved
+ * Return: 1 0r 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int x;
+
+if (index > sizeof(size_t) * 8)
+{
+return (-1);
+}
+for (x = 0; x < index ; x++)
+{
+n = n >> 1;
+}
+return (n & 1);
+}
