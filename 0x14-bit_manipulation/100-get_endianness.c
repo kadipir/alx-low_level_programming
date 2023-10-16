e3f7b074ff4f9001dd032331893aba798227272b
truth <truth@gmail.com>
2023-10-15 19:44:31 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_endianness - entry point
+ * Return: 1 or 0
+ */
+int get_endianness(void)
+{
+unsigned long int index = 1;
+char *data = (char*)&index;
+
+if (*data == 1)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
