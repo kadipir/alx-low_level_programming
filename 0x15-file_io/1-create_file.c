c71f78d1244d406d833e77f395280b32f667d22d
truth <truth@gmail.com>
2023-10-19 19:36:30 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - entry point
  * @filename: name of the file
  * @text_content: content of the file
- *
+ * Return: 1 or 0
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -11,7 +11,7 @@ int fd;
 int letters;
 int rwr;
 
-if (!filename)
+if (filename == NULL)
 {
 return (-1);
 }
